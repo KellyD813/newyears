44af4777c3e8f6eb490c2c2963555e93470f6725
Kelly Devine <kellyd813@gmail.com>
2017-08-21 16:52:20 -0400

    Working on styling for mobile

--- public/main.js
+++ public/main.js
@@ -22,3 +22,29 @@ tabs.forEach(function(tab) {
     });
   }
 });
+
+// -- Navigation on Mobile --
+var homeTab = document.querySelector('.home-link'),
+    aboutTab = document.querySelector('.about-link'),
+    partTab = document.querySelector('.participants-link'),
+    partLinks = document.querySelectorAll('.person');
+
+partTab.addEventListener('click', function() {
+  homeTab.classList.remove('underline');
+  aboutTab.classList.remove('underline');
+  partTab.classList.add('underline');
+
+  partLinks.forEach(function(person) {
+    person.classList.add('block');
+  });
+});
+
+if (location.pathname === '/') {
+  homeTab.classList.add('underline');
+  partTab.classList.remove('underline');
+} else if (location.pathname === '/about') {
+  aboutTab.classList.add('underline');
+  partTab.classList.remove('underline');
+} else if (location.pathname.includes('people')) {
+  partTab.classList.add('underline');
+}
